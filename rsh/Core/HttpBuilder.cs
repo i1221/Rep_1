0568b2c5aa8d64232538a05f282dac99061401e2
i1221 <i122123@yandex.by>
2019-07-12 10:58:41 +0300

    Add_Win_Git

---
+++ rsh/Core/HttpBuilder.cs
@@ -0,0 +1,38 @@
+﻿using SimpleHttpServer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer
+{
+    class HttpBuilder
+    {
+        public static HttpResponse InternalServerError()
+        {
+            string content = File.ReadAllText("Core/Resources/Pages/500.html"); 
+
+            return new HttpResponse()
+            {
+                ReasonPhrase = "InternalServerError",
+                StatusCode = "500",
+                ContentAsUTF8 = content
+            };
+        }
+
+        public static HttpResponse NotFound()
+        {
+            // string content = File.ReadAllText("Core/Resources/Pages/404.html");
+            string content = File.ReadAllText("Core/Resources/Pages/404.html");
+
+            return new HttpResponse()
+            {
+                ReasonPhrase = "NotFound",
+                StatusCode = "404",
+                ContentAsUTF8 = content
+            };
+        }
+    }
+}
---
+++ rsh/Core/ViberApi/Models/WebhookModels/WebhookSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsh.Core.ViberApi.Models.WebhookModels
+{
+    class WebhookSettings
+    {
+        public string Url { get; set; }
+    }
+}
